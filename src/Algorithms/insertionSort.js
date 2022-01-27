3549336fa0c9366c178537845faaeba09f941fa3
Suraj Pathak <surajpathak2057@gmail.com>
2022-01-27 12:48:43 +0545

    Add Insertion sort animation with color

--- src/Algorithms/insertionSort.js
+++ src/Algorithms/insertionSort.js
@@ -6,10 +6,18 @@ function insertionSortAnimation(array) {
   for (let i = 1; i < n; i++) {
     key = { ...rects[i] };
     for (j = i - 1; j >= 0 && rects[j].height > key.height; j--) {
-      rects[j + 1] = { ...rects[j] }; // SWAP
+      rects[j] = {...rects[j], isSorting: true}; // is being checked color: red
+      rects[j + 1] = { ...rects[j], isSorting: false}; // SWAP
       results.push(rects.slice());
+      rects[j] = {...rects[j], isSorting: false}; // finished checking color: default
     }
     rects[j + 1] = { ...key};
+  }
+  // After sorting is complete
+  // make bars starting from index 0 to last green one at a time to show sorted complete animation
+  for (let i=0; i<n; i++) {
+      rects[i] = {...rects[i], isSorted: true};
+      results.push(rects.slice());
   }
   results.push(rects.slice()); // push final result
   return results;
