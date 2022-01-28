11de7993fbbd7943613b455c207970eff13d9693
Suraj Pathak <surajpathak2057@gmail.com>
2022-01-28 23:06:09 +0545

    Add animation for quick sort

--- src/Algorithms/quickSort.js
+++ src/Algorithms/quickSort.js
@@ -3,17 +3,34 @@ export default function quickSortAnimation(array) {
   let results = []; // to store array at each iteration
   let rects = array.slice(); // create fresh copy
 
+  function saveAnimation(index1, index2) {
+    rects[index1] = {...rects[index1], isSorting: true};
+    rects[index2] = {...rects[index2], isSorting: true};
+    results.push(rects.slice());  // store animation
+    rects[index1] = {...rects[index1], isSorting: false};
+    rects[index2] = {...rects[index2], isSorting: false};
+  }
+
   function partition(rects, lb, ub) {
     let start = lb;
     let end = ub;
     const pivot = rects[lb].height;
     while (start < end) {
-      while (rects[start].height <= pivot && start < ub) start++;
-      while (rects[end].height > pivot && end > lb) end--;
+      while (rects[start].height <= pivot && start < ub) {
+          saveAnimation(start, end);
+          start++;
+      }
+      while (rects[end].height > pivot && end > lb) {
+          saveAnimation(start, end);
+          end--;
+      }
       if (start < end) [rects[start], rects[end]] = [rects[end], rects[start]]; // SWAP
-      results.push(rects.slice());  // store animation
+      saveAnimation(start, end);
     }
+    saveAnimation(lb, end);
     [rects[lb], rects[end]] = [rects[end], rects[lb]]; // Swap pivot to its correct position
+    rects[end] = {...rects[end], isSorted: true}; // pivot is sorted so change color to green
+    results.push(rects.slice());
     return end;
   }
 
@@ -23,6 +40,13 @@ export default function quickSortAnimation(array) {
       let pi = partition(rects, lb, ub);
       quickSortAlgo(rects, lb, pi - 1);
       quickSortAlgo(rects, pi + 1, ub);
+    } else {
+        // if the partition is complete this condition runs
+        // make all bars of this partition isSorted: true to change color to green
+        for (let i=ub; i<=lb; i++) {
+            rects[i] = {...rects[i], isSorted: true};
+            results.push(rects.slice());
+        }
     }
   }
 
@@ -30,3 +54,10 @@ export default function quickSortAnimation(array) {
   results.push(rects.slice()); // push final result
   return results;
 }
+
+
+/* Note:
+here we keep track of start and end index with red color
+when pivot is set to its correct position change its color to green
+when any partition is completely sorted change all its bar to green
+*/
